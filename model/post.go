44faf0ded486b30f46ae2275474f8cdb3b06dc62
Alex Holmes <alex@sed.dev>
2024-08-22 19:49:53 -0400

    Create db models

--- model/post.go
+++ model/post.go
@@ -1,1 +1,20 @@
 package model
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type Post struct {
+	Id             uuid.UUID `json:"id"`
+	OwnerId        uuid.UUID `json:"owner_id"`
+	Title          string    `json:"title"`
+	Slug           string    `json:"slug"`
+	Description    string    `json:"description"`
+	Content        string    `json:"content"`
+	Created        time.Time `json:"created"`
+	Updated        time.Time `json:"updated"`
+	Hidden         bool      `json:"hidden"`
+	Deleted        bool      `json:"deleted"`
+	RecoveryWindow time.Time `json:"recovery_window"`
+}
--- model/user.go
+++ model/user.go
@@ -1,1 +1,14 @@
 package model
+
+import (
+	"github.com/google/uuid"
+)
+
+type User struct {
+	Id        uuid.UUID `json:"id"`
+	UserName  string    `json:"user_name"`
+	Password  string    `json:"password"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+}
